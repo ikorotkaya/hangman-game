502def9ddc02078ba34e7ff1435b0ac75ca025db
ikorotkaya <irinkakorotkaya@gmail.com>
2023-06-16 10:18:19 +0200

    Use state to get random word

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
+import { useState } from 'react'
+import words from './wordList.json'
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  });
+
+  console.log(wordToGuess)
+
   return <h1>Hi</h1>
 }
 
