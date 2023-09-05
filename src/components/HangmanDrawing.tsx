e9996b094d4220106907ccbd87d287065594f999
ikorotkaya <irinkakorotkaya@gmail.com>
2023-09-05 13:16:17 +0200

    Refactor repeated lines of code

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,106 +1,90 @@
-const HEAD = (
+import { CSSProperties, Key } from "react";
+
+const createBodyPart = (styles: CSSProperties | undefined, key: Key | null | undefined) => (
   <div
     style={{
-      width: "70px",
-      height: "70px",
-      borderRadius: "100%",
-      border: "10px solid black",
       position: "absolute",
-      top: "50px",
-      right: "-30px",
+      ...styles,
     }}
-    data-testid='head'
+    data-testid={`body-part-${key}`}
     role="body-part"
-    key={"head"}
+    key={key}
   />
 );
 
-const BODY = (
-  <div
-    style={{
-      width: "10px",
-      height: "100px",
-      background: "black",
-      position: "absolute",
-      top: "120px",
-      right: 0,
-    }}
-    data-testid='body'
-    role="body-part"
-    key={"body"}    
-  />
+const HEAD = createBodyPart(
+  {
+    width: "70px",
+    height: "70px",
+    borderRadius: "100%",
+    border: "10px solid black",
+    top: "50px",
+    right: "-30px",
+  },
+  "head"
 );
 
-const RIGHT_ARM = (
-  <div
-    style={{
-      width: "100px",
-      height: "10px",
-      background: "black",
-      position: "absolute",
-      top: "150px",
-      right: "-100px",
-      rotate: "-30deg",
-      transformOrigin: "left bottom",
-    }}
-    data-testid='right-arm'
-    role="body-part"
-    key={"right-arm"}
-  />
+const BODY = createBodyPart(
+  {
+    width: "10px",
+    height: "100px",
+    background: "black",
+    top: "120px",
+    right: 0,
+  },
+  "body"
 );
 
-const LEFT_ARM = (
-  <div
-    style={{
-      width: "100px",
-      height: "10px",
-      background: "black",
-      position: "absolute",
-      top: "150px",
-      right: "10px",
-      rotate: "30deg",
-      transformOrigin: "right bottom",
-    }}
-    data-testid='left-arm'
-    role="body-part"
-    key={"left-arm"}
-  />
+const RIGHT_ARM = createBodyPart(
+  {
+    width: "100px",
+    height: "10px",
+    background: "black",
+    top: "150px",
+    right: "-100px",
+    rotate: "-30deg",
+    transformOrigin: "left bottom",
+  },
+  "right-arm"
 );
 
-const RIGHT_LEG = (
-  <div
-    style={{
-      width: "100px",
-      height: "10px",
-      background: "black",
-      position: "absolute",
-      top: "210px",
-      right: "-90px",
-      rotate: "60deg",
-      transformOrigin: "left bottom",
-    }}
-    data-testid='right-leg'
-    role="body-part"
-    key={"right-leg"}
-  />
+const LEFT_ARM = createBodyPart(
+  {
+    width: "100px",
+    height: "10px",
+    background: "black",
+    top: "150px",
+    right: "10px",
+    rotate: "30deg",
+    transformOrigin: "right bottom",
+  },
+  "left-arm"
 );
 
-const LEFT_LEG = (
-  <div
-    style={{
-      width: "100px",
-      height: "10px",
-      background: "black",
-      position: "absolute",
-      top: "210px",
-      right: 0,
-      rotate: "-60deg",
-      transformOrigin: "right bottom",
-    }}
-    data-testid='left-leg'
-    role="body-part"
-    key={"left-leg"}
-  />
+const RIGHT_LEG = createBodyPart(
+  {
+    width: "100px",
+    height: "10px",
+    background: "black",
+    top: "210px",
+    right: "-90px",
+    rotate: "60deg",  
+    transformOrigin: "left bottom",
+  },
+  "right-leg"
+);
+
+const LEFT_LEG = createBodyPart(
+  {
+    width: "100px",
+    height: "10px",
+    background: "black",
+    top: "210px",
+    right: 0,
+    rotate: "-60deg",
+    transformOrigin: "right bottom",
+  },
+  "left-leg"
 );
 
 const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
